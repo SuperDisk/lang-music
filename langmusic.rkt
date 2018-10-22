4874d68210dba38c6f5d4bfe488dcab5b56b01c5
Nick <yux50000@hotmail.com>
2018-10-21 21:38:16 -0400

    Got stream stuff working

--- langmusic.rkt
+++ langmusic.rkt
@@ -10,48 +10,43 @@
 (define (repeat num ls)
   (build-list num (const ls)))
 
-;; We have to define our own custom stream functions because the built-in Racket ones are trash.
+;; We have to define our own custom stream functions because the built-in Racket ones
+;; don't let you have "interesting" tails.
 (define empty$ null)
 (define empty$? null?)
 (define-syntax-rule (cons$ x y)
-  (cons x (thunk y)))
+  (cons x (delay y)))
 (define car$ car)
-(define (cdr$ $)
-  (let ((x ((cdr $))))
-    (if (procedure? x) (x) x)))
-(define (append$* $1 $2)
-  (thunk
-    (let (($1 ($1))
-          ($2 ($2)))
-      (cond
-        ((empty$? $1) $2)
-        ((empty$? $2) $1)
-        (else
-         (cons$ (car$ $1) ((append$* (thunk (cdr$ $1)) (thunk $2)))))))))
+(define (cdr$ $) (force (cdr $)))
+(define (infinitize $)
+  (let recur ((sub$ $))
+    (cond
+      ((empty$? sub$) (infinitize $))
+      (else (cons$ (car$ sub$) (recur (cdr$ sub$)))))))
 
-(define-syntax-rule (append$ $1 $2)
-  (let ()
-    (define x (append$* (thunk $1) (thunk $2)))
-    (x)))
-
-(define s1 (cons$ 1 (cons$ 2 null)))
-(define s2 (append$ s1 s2))
+(define (append$ . strs)
+  (define (_append2$ $1 $2)
+    (cond
+      ((empty$? $1) $2)
+      ((empty$? $2) $1)
+      (else
+       (cons$ (car$ $1) (append$ (cdr$ $1) $2)))))
+  (foldr _append2$ empty$ strs))
+(define (take$ $ n)
+  (cond
+    ((zero? n) empty$)
+    (else (cons (car$ $) (take$ (cdr$ $) (sub1 n))))))
+(define (stream$ . items)
+  (let stream$^ ((items items))
+    (cond
+      ((empty? items) empty$)
+      (else (cons$ (car items) (stream$^ (cdr items)))))))
+(define stream$* (curry apply stream$))
 
 (define (play-note midi-num duration)
   (let ((real-note (+ midi-num (* 12 (octave)))))
     (displayln (format "Playing note ~a, duration: ~a, octave: ~a, voice: ~a"
                        real-note duration (octave) (voice)))))
-
-(define (generator-append . seqs)
-  (define (yieldall gen)
-    (let recur ()
-      (let ((out (gen)))
-        (when (not (void? out))
-          (yield out)
-          (recur)))))
-  (generator ()
-             (for ((seq seqs))
-               (yieldall seq))))
 
 (define-for-syntax (make-note-symbol s1 s2)
   (string->symbol
@@ -71,11 +66,9 @@
                                        (make-note-symbol note-base duration))))
                          #`(begin
                              (define note-id
-                               (sequence->generator
-                                (list
-                                 (thunk (play-note
-                                         midi-base
-                                         #,(string->number real-duration))))))
+                               (stream$ (thunk (play-note
+                                                midi-base
+                                                #,(string->number real-duration)))))
                              (provide note-id)))))
                    '("" "2" "4" "8" "16" "32"))))
        #`(begin
@@ -84,15 +77,15 @@
 (define-syntax defseq
   (syntax-parser
     ((_ name seq ...)
-     #`(define (name)
-         (generator-append seq ...)))))
+     #`(define name
+         (append$ seq ...)))))
 
 (define (loop num . seqs)
-  (let ((bigseq (apply generator-append seqs)))
+  (let ((bigseq (apply append$ seqs)))
     (cond
-      ((positive? num) (apply generator-append (repeat num bigseq)))
-      ((zero? num)
-       ))))
+      ((zero? num) (infinitize bigseq))
+      ((positive? num) (apply append$ (repeat num bigseq)))
+      (else (error "Can't loop with a negative number!")))))
 
 (define-note-divisions g# 32)
 (define-note-divisions g 31)
