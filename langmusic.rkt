911b31cbe5487de617bd0ac8c7332b400ef250e8
Nick <yux50000@hotmail.com>
2018-10-22 13:03:04 -0400

    Removing custom stream stuff and using real racket streams. Guess i was wrong!

--- langmusic.rkt
+++ langmusic.rkt
@@ -1,8 +1,6 @@
 #lang racket
 
-(require (for-syntax syntax/parse racket/syntax racket/generator)
-         racket/generator
-         compatibility/defmacro)
+(require (for-syntax syntax/parse racket/syntax))
 
 (define octave (make-parameter 0))
 (define voice (make-parameter 0))
@@ -10,48 +8,28 @@
 (define (repeat num ls)
   (build-list num (const ls)))
 
-;; We have to define our own custom stream functions because the built-in Racket ones
-;; don't let you have "interesting" tails.
-(define empty$ null)
-(define empty$? null?)
-(define-syntax-rule (cons$ x y)
-  (cons x (delay y)))
-(define car$ car)
-(define (cdr$ $) (force (cdr $)))
 (define (infinitize $)
   (let recur ((sub$ $))
     (cond
-      ((empty$? sub$) (infinitize $))
-      (else (cons$ (car$ sub$) (recur (cdr$ sub$)))))))
+      ((stream-empty? sub$) (infinitize $))
+      (else (stream-cons (stream-first sub$) (recur (stream-rest sub$)))))))
 
-(define (append$ . strs)
-  (define (_append2$ $1 $2)
-    (cond
-      ((empty$? $1) $2)
-      ((empty$? $2) $1)
-      (else
-       (cons$ (car$ $1) (append$ (cdr$ $1) $2)))))
-  (foldr _append2$ empty$ strs))
-(define (map$ fun $)
+(define (stream-take $ n)
   (cond
-    ((empty$? $) empty$)
-    (else
-     (cons$ (fun (car$ $)) (map$ fun (cdr$ $))))))
-(define (take$ $ n)
-  (cond
-    ((zero? n) empty$)
-    (else (cons (car$ $) (take$ (cdr$ $) (sub1 n))))))
-(define (stream$ . items)
-  (let stream$^ ((items items))
-    (cond
-      ((empty? items) empty$)
-      (else (cons$ (car items) (stream$^ (cdr items)))))))
-(define stream$* (curry apply stream$))
+    ((zero? n) empty-stream)
+    (else (cons (stream-first $) (stream-take (stream-rest $) (sub1 n))))))
 
 (define (play-note midi-num duration)
   (let ((real-note (+ midi-num (* 12 (octave)))))
     (displayln (format "Playing note ~a, duration: ~a, octave: ~a, voice: ~a"
                        real-note duration (octave) (voice)))))
+
+(define-syntax punk
+  (syntax-parser
+    ((_ ((id val) ...) code ...)
+     #'(thunk
+        (parameterize ((id val) ...)
+          code ...)))))
 
 (define-for-syntax (make-note-symbol s1 s2)
   (string->symbol
@@ -71,7 +49,7 @@
                                        (make-note-symbol note-base duration))))
                          #`(begin
                              (define note-id
-                               (stream$ (thunk (play-note
+                               (stream (thunk (play-note
                                                 midi-base
                                                 #,(string->number real-duration)))))
                              (provide note-id)))))
@@ -84,24 +62,24 @@
     ((_ name (id ...) code ...)
      #'(define (name id ...)
          (let* ((seqs (begin code ...)))
-           (apply append$ seqs))))))
+           (apply stream-append seqs))))))
 
 (define-syntax defseq
   (syntax-parser
     ((_ name seq ...)
      #`(define name
-         (append$ seq ...)))))
+         (stream-append seq ...)))))
 
 (define (loop num . seqs)
-  (let ((bigseq (apply append$ seqs)))
+  (let ((bigseq (apply stream-append seqs)))
     (cond
       ((zero? num) (infinitize bigseq))
-      ((positive? num) (apply append$ (repeat num bigseq)))
+      ((positive? num) (apply stream-append (repeat num bigseq)))
       (else (error "Can't loop with a negative number!")))))
 
 (define (octave+ num . seqs)
-  (let ((bigseq (apply append$ seqs)))
-    (map$ identity bigseq)))
+  (let ((bigseq (apply stream-append seqs)))
+    (stream-map identity bigseq)))
 
 (define (together . seqs)
   (displayln "Somehow combine them"))
@@ -122,8 +100,8 @@
 (define-note-divisions a# 22)
 (define-note-divisions a 21)
 
-(define > (stream$ (thunk (displayln "Bumping octave up"))))
-(define < (stream$ (thunk (displayln "Bumping octave down"))))
+(define > (stream (thunk (displayln "Bumping octave up"))))
+(define < (stream (thunk (displayln "Bumping octave down"))))
 
 (defseq bassline
   c d# f g)
@@ -131,7 +109,7 @@
 (defun random-phrase (num-notes)
   (let* ((ls (list a b c d e f g))
         (len (length ls)))
-    (build-list num-notes (λ _ (list-ref ls (random len))))))
+    (build-list num-notes (thunk* (list-ref ls (random len))))))
 
 (defseq phrase1
   c d e f g4 a16 b16 > (random-phrase 8) <)
