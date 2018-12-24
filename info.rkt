41fdadb7bedf2a98146914a2b80a55a36f7084c7
Levi Campbell <levicc00123@gmail.com>
2018-12-24 06:36:20 -0700

    Created info file and added gitignore
    This fixes issue #1

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "lang-music")
+(define deps '("base" "binaryio-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/hamlet.scrbl" ())))
+(define pkg-desc "A music notation language ")
+(define version "0.0")
+(define pkg-authors "SuperDisk")
